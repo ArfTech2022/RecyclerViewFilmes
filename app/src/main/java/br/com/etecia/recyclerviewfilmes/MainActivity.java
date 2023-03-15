28b64531917a143b1f5c797b491126c2af4d0ae0
ArfTech2022 <armando.rodrigues@etec.sp.gov.br>
2023-03-14 22:12:49 -0300

    criando classe adptadora e pasando a base de dados

--- app/src/main/java/br/com/etecia/recyclerviewfilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/MainActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<Filmes> listaFilmes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //criando a base de dados para carregamento da lista de filmes
+        listaFilmes = new ArrayList<>();
+
+        listaFilmes.add(
+                new Filmes("A Queda",
+                        "Para as melhores amigas Becky e Hunter, a vida é sobre vencer medos e ultrapassar limites.",
+                        "Aventura",
+                        R.drawable.A_queda));
+
+        //criando (instanciando) a classe adptadora e passando os parâmetros
+        MyAdapter adapter = new MyAdapter(getApplicationContext(),);
     }
 }
